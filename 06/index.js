3521044190c2006c8063ee2e757aaa90c4ecf4f8
Piotr Palarz <piotr@depalage.com>
2020-07-29 15:44:54 +0200

    06-finish

---
+++ 06/index.js
@@ -0,0 +1,23 @@
+let number1 = 2;
+let number2 = 4;
+
+const res1 = number1 + number2;
+
+const res2 = "2" + "43";
+
+const res3 = number1 - number2;
+
+const res4 = number1 * number2;
+
+const res5 = number1 / number2;
+
+const res6 = 6 % 3;
+const res7 = 6 % 4;
+
+const res8 = 2 + 2 * 4;
+const res9 = (2 + 2) * 4;
+
+// number1 = number1 + 2;
+number1 += 2;
+
+console.log(number1);
