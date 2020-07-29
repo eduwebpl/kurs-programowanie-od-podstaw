647e9a166333f3faac2c4e382e3a6b5af67d1a34
Piotr Palarz <piotr@depalage.com>
2020-07-29 14:56:32 +0200

    02-finish

---
+++ 02/index.js
@@ -0,0 +1,7 @@
+const firstName = "Jan";
+const lastName = 'Kowalski';
+const bookTitle = '"Władca Pierścieni"';
+// const multiline = "linia pierwsza\nlinia druga";
+const multiline = `Tytuł książki: ${bookTitle}`;
+
+console.log(multiline);
