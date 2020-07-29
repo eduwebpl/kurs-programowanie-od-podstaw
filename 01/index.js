db92418a9a5936348f9954a2d1d7301dc080ce4c
Piotr Palarz <piotr@depalage.com>
2020-07-29 14:39:58 +0200

    01-finish

---
+++ 01/index.js
@@ -0,0 +1,14 @@
+let imie = "Jan";
+const nazwisko = "Kowalski";
+
+console.log(imie);
+
+imie = "Marcin";
+
+console.log(imie);
+
+// nazwisko = "Nowak";
+
+let kopiaImie = imie;
+
+console.log(kopiaImie);
