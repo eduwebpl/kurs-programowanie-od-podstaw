2c1545a4e4c7b382f5c64e562bf1246b26ca158e
Piotr Palarz <piotr@depalage.com>
2020-07-29 15:14:56 +0200

    04-finish

---
+++ 04/index.js
@@ -0,0 +1,21 @@
+const isLoggedIn = false;
+
+if(isLoggedIn) {
+    console.log("Zalogowano");
+} else {
+    console.log("Nie zalogowano");
+}
+
+const value = "";
+
+// Wartość fałszywe
+// false
+// 0
+// ""
+// null
+// undefined
+// NaN
+
+if(value) {
+    console.log("Ta wartość jest prawdziwa!");
+}
