ee1cd8a14c03f70598f8d18ecb067ff683f99552
Piotr Palarz <piotr@depalage.com>
2020-07-31 17:55:38 +0200

    26-finish

---
+++ 26/index.js
@@ -0,0 +1,17 @@
+const firstName = "Maciej";
+
+function init() {
+    const firstName = "Piotr";
+
+    console.log(firstName);
+}
+
+init();
+
+console.log(firstName);
+
+if(firstName) {
+    const lastName = "Kowalski";
+}
+
+console.log(lastName);
